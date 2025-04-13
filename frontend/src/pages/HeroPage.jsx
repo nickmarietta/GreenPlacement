805371462f6e6ffe220fa067f3c67ea856dc4e2f
mattmarietta <mattmarietta24@gmail.com>
2025-04-13 13:36:46 -0700

    updated hero

--- frontend/src/pages/HeroPage.jsx
+++ frontend/src/pages/HeroPage.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import NebulaBG from '../components/Nebula';
+import React, { useState } from 'react';
+import { motion } from "framer-motion"; // make sure this is correct
 import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
 import earthImg from '../assets/planet-earth.png';
-import astronautImg from '../assets/astronaut.png';
+import NebulaBackground from '../components/Nebula';
 
 const HeroPage = () => {
   const [zoom, setZoom] = useState(false);
@@ -16,43 +15,24 @@ const HeroPage = () => {
     }, 2000);
   };
 
-
   return (
-    <div className="relative h-screen w-full overflow-hidden bg-gradient-to-t from-[#050819] to-[#2e1753]">
-      <NebulaBG />
-
-      <div
-        className="absolute bottom-10 left-10 flex items-center space-x-2"
-        style={{ zIndex: 30 }}
-      >
-        <img
-          src={astronautImg}
-          alt="Astronaut"
-          className="w-24 animate-rotate-astronaut"
+    <div>
+    <NebulaBackground />
+    <div className="relative w-full h-screen overflow-hidden" >
+      <div className="flex items-center justify-center h-full">
+        <motion.img
+          src={earthImg}
+          className="w-[200px] h-[200px] cursor-pointer z-20"
+          initial={{ scale: 1 }}
+          animate={{scale: zoom ? 20 : 1, opacity: 1}}
+          transition={{ duration: 3, ease: "easeInOut" }}
+          whileHover={{scale:1.5, rotateZ: 100}}
+          onClick={handleClick}
         />
-        <div className="chat chat-start">
-        <div className="chat-bubble chat-bubble-info">Please save the environment!</div>
       </div>
       </div>
 
-      <img
-        src={earthImg}
-        alt="Earth"
-        onClick={handleClick}
-        className={`cursor-pointer transition-transform duration-2000 ease-in-out ${
-          zoom ? 'scale-[500] z-50' : 'scale-100'
-        }`}
-        style={{
-          position: 'absolute',
-          top: '50%',
-          left: '50%',
-          transform: 'translate(-50%, -50%)',
-          transformOrigin: 'center center',
-          zIndex: 20,
-        }}
-      />
-
-      <div className="absolute top-[10%] text-center text-white z-10 w-full">
+      <div className="absolute top-[10%] text-center text-white z-30 w-full">
         <h1 className="font-bold text-green-500 text-5xl">Green Placement</h1>
       </div>
     </div>
